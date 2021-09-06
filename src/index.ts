72db2ea3994e394e813397e0c8e435c3da338253
abschill <ajay198810@gmail.com>
2021-09-06 09:24:48 -0400

    0.0.1

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import Template from './prerender/template';
 import Partial from "./prerender/partials";
 
 const p = new Partial( './views/layout/head.html', { order:0 } )
-const p0 = new Partial( './views/layout/footer.html', { order: 1 } );
+const p0 = new Partial( './views/layout/footer.html', { order: 1 }, [{ foo:'bar' }] );
 const t = new Template( [p, p0] , 'ssr' );
-//console.log( );
+console.log( p0.varList );
 t.render()
--- src/prerender/partials/index.ts
+++ src/prerender/partials/index.ts
@@ -5,11 +5,17 @@ export default class Partial {
     path: string
     type: PARTIAL_TYPE
     content: string
+    varList?: Object[]
 
-    constructor( path: string, type:PARTIAL_TYPE ){
+    constructor( path: string, type:PARTIAL_TYPE, varList?: Object[] ){
         this.path = path;
         this.type = type;
+        this.varList = varList;
         this.content = fs.readFileSync( this.path ).toString( 'utf-8' );
+    }
+
+    parseVars(){
+        console.log( this.varList );
     }
 
     asBuffer(){
@@ -17,7 +23,6 @@ export default class Partial {
     }
 
     asObject(){
-        //console.log( this.type );
         return {
             type: this.type, 
             html: this.content
