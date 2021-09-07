554b0a50c347b8b1084f40118dc35b2c7722f06a
abschill <ajay198810@gmail.com>
2021-09-07 10:09:01 -0400

    fix example delimiter

--- src/parser/util.ts
+++ src/parser/util.ts
@@ -17,7 +17,6 @@ export function renderVars ( pt: string, varList: Object[] ){
     let _copy = pt;
     const out = varList.map( item => Object.entries( item ) );
     const _vrs:Array<string>[] = out.flat();
-
     _vrs.forEach( vr => {
         const _replace = `<!--<${config._internals.delimiter}=${vr[0]} />-->`;
         _copy = _copy.replace( _replace , vr[1])
