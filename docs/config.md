40b0175bdca072502f12df5312c93e72d1831167
abschill <ajay198810@gmail.com>
2021-10-01 14:19:58 -0400

    0.0.38 config

---
+++ docs/config.md
@@ -0,0 +1,9 @@
+# Configuration
+
+_static_config
+
+This property in package.json allows you to set up a configuration for the static site generation utility contained in the CLI. 
+
+_partial_data 
+
+This property in package.json allows you to declare your partial variables 
--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 const Loader  = require( './dist' );
-const packagedData = require( './package.json' )._template_data;
+const partialData = require( './package.json' )._partial_data;
+const packagedData = require( './config.json' )._template_data;
 const Handler = new Loader({
-     root: 'views',
-     templates: 'pages',
-     partials: 'partials',
-     _partialInput: {
-         head: {
-            title: 'Hello World',
-            desc: 'Cool Description Bro',
-        },
-        footer: {
-            title: 'Hello From Footer'
-        }
-     }
+     _partialInput: partialData
 });
 
-// console.log( Handler );
 // Server Side Rendering
-console.log( Handler.getTemplate( 'home', packagedData ) );
+console.log( Handler.getTemplate( 'home', packagedData ) )
+
 console.log('\n' );
 
-//Simple Static Pages
+//Simple Hard-Coded Pages
 console.log( Handler.getTemplate( 'about' ) );
