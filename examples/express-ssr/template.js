30435364bbe0d95b536dca9b5e0d0a92c729dcee
abschill <ajay198810@gmail.com>
2021-10-16 00:59:12 -0400

    update ex 0.0.93

--- examples/express-ssr/template.js
+++ examples/express-ssr/template.js
@@ -1,11 +1,14 @@
 module.exports = () => {
     //load data
     return { 
-        content: 'Body Content', 
-        items: [ 'foo', 'bar' ], 
-        items2: [ 
-            { title: 'item 1', desc: 'this is item 1' }, 
-            { title: 'item 2', desc: 'this is item 2' } 
-        ] 
+        home:{
+            content: 'Body Content', 
+            items: [ 'foo', 'bar' ], 
+            items2: [ 
+                { title: 'item 1', desc: 'this is item 1' }, 
+                { title: 'item 2', desc: 'this is item 2' } 
+            ] 
+        }
+        
     }
 }
