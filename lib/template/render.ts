f374289f225175b72b925aae22b11730cfe89057
abschill <ajay198810@gmail.com>
2021-10-15 21:06:58 -0400

    0.0.82

--- lib/template/render.ts
+++ lib/template/render.ts
@@ -20,6 +20,12 @@ export default function render( _varList:Object, inp: string, config: Loader ) {
                 const _iterator = iterators[idx - 1];
                 const match = Object.entries( _varList )[ idx ];
                 if( p && p.includes( 'render' ) ) {
+                    if( config.verbose ) {
+                        console.log( 'To Insert: \n' );
+                        console.log( match[1] );
+                        console.log( '\nAt:' );
+                        console.log( p );
+                    }
                     _copy = insertValue( _copy, p, match[1] );
                 }
                 else{ 
@@ -42,8 +48,21 @@ export default function render( _varList:Object, inp: string, config: Loader ) {
                     }
                 }
             } );
+            if( config.verbose ) {
+                console.log( 'Out Values:\n' );
+                console.log( outVal );
+                console.log( 'Out Objects:\n' );
+                console.log( outObj );
+            }
             const elArr:string = outVal.map( x => x.child ).join( '' );
             const valArr:string = outObj.map( x => x.child ).join( '' );
+            
+            if( config.verbose ) {
+                console.log( 'Element Array:\n' );
+                console.log( elArr );
+                console.log( '\nValue Array:\n' );
+                console.log( valArr );
+            }
             outVal.forEach( ( _out ) => _copy = insertValue( _copy,  _out.parent, elArr ) );
             outObj.forEach( ( _out ) => _copy = insertValue( _copy,  _out.parent, valArr ) );
         }
